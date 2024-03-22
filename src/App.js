5dd128d2ea3eedf2009b809bf3d4a6b6561a8ae6
saurabhkumarr99 <saurabhkumarr99@gmail.com>
2024-03-22 20:14:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProviderSKR } from './allCompontsSKR/AuthContextSKR';
+import HeaderSKR from './allCompontsSKR/HeaderSKR';
+import HomeSKR from './allCompontsSKR/HomeSKR';
+import LoginSKR from './allCompontsSKR/LoginSKR';
+import ContactUsSKR from './allCompontsSKR/ContactUsSKR';
+import AboutUsSKR from './allCompontsSKR/AboutUsSKR';
+import RecipeExplorerSKR from './allCompontsSKR/RecipeExplorerSKR';
+import { Provider } from 'react-redux';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AuthProviderSKR>
+          <BrowserRouter >
+            <HeaderSKR />
+            <Routes>
+              <Route path="/" element={<HomeSKR />} />
+              <Route path="/homeSKR" element={<HomeSKR />} />
+              <Route path="/loginSKR" element={<LoginSKR />} />     
+              <Route path="/ContactUsSKR" element={<ContactUsSKR />} />
+              <Route path="/aboutusSKR" element={<AboutUsSKR />} />
+              <Route path="/recipeSKR" element={<RecipeExplorerSKR />} />
+            </Routes>
+          </BrowserRouter>
+      </AuthProviderSKR>
   );
 }
 
 export default App;
+
